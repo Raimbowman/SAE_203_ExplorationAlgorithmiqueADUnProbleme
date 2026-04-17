830884022077d1e8f845e0bc2d16cc25a55e61dd
Raimbowman <noamlacombe12@gmail.com>
2026-04-17 09:38:36 +0200

    crée les deux constructeurs (non terminés)

---
+++ src/iut/info1/sae202/Polynome.java
@@ -0,0 +1,22 @@
+/*
+ * Polynome.java					17/04/2026
+ * IUT de Rodez, BUT1 2025-2026, pas de copyright
+ */
+package iut.info1.sae202;
+
+/**
+ * Polynôme sous la forme Kx^n + Kx^n-1 + ... + Kx^0
+ * avec K un réel quelconque
+ * @author Noam LACOMBE
+ */
+public class Polynome {
+	
+	public Polynome (int[] coefficients) {
+		
+	}
+	
+	public Polynome () {
+		
+	}
+	
+}
